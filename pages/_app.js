01c077f932c92a5eaa75dc04f92df9e203deb85a
yanoyuto <yanoyuto.0724@gmail.com>
2025-04-12 10:42:26 +0900

    アナリティクス追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import "@/styles/globals.css";
+import Script from 'next/script'
+import '../styles/globals.css' // もともとあるスタイルのインポートなど
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JR10FNNW5N"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JR10FNNW5N', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
+
